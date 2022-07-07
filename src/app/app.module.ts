238268dfa171c688930c8aedfaa8550f74b09640
pouyababaie <pouyababaie123@2gmail.com>
2022-07-08 02:39:26 +0430

    routing deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SimpleReactiveFormComponent } from './simple-reactive-form/simple-reactive-form.component';
 
 @NgModule({
   declarations: [AppComponent, SimpleReactiveFormComponent],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, FormsModule],
+  imports: [BrowserModule, ReactiveFormsModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
