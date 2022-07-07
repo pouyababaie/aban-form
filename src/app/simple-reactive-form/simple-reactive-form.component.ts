d9f2555a46ca2268c34b5212387a1fa2bc4501ad
pouyababaie <pouyababaie123@2gmail.com>
2022-07-08 02:37:57 +0430

    reactive-form component added

---
+++ src/app/simple-reactive-form/simple-reactive-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-reactive-form',
+  templateUrl: './simple-reactive-form.component.html',
+  styleUrls: ['./simple-reactive-form.component.scss']
+})
+export class SimpleReactiveFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
