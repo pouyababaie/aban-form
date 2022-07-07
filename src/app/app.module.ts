47918ecb473193d25323a2a36676d888c4d39bd7
pouyababaie <pouyababaie123@2gmail.com>
2022-07-08 02:38:53 +0430

    new modules added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,15 +7,9 @@ import { AppComponent } from './app.component';
 import { SimpleReactiveFormComponent } from './simple-reactive-form/simple-reactive-form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SimpleReactiveFormComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, SimpleReactiveFormComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
