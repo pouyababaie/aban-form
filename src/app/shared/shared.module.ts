a17e95abba260d2589aa41cb37c1a92dd42340f3
pouyababaie <pouyababaie123@2gmail.com>
2022-07-10 15:47:18 +0430

    shared module updated

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { SimpleReactiveFormComponent } from '../simple-reactive-form/simple-reactive-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InputTextModule } from 'primeng/inputtext';
+import { DropdownModule } from 'primeng/dropdown';
+import { CalendarModule } from 'primeng/calendar';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { InputNumberModule } from 'primeng/inputnumber';
+import {CheckboxModule} from 'primeng/checkbox';
+import {InputTextareaModule} from 'primeng/inputtextarea';
+
+@NgModule({
+  declarations: [SimpleReactiveFormComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    InputTextModule,
+    DropdownModule,
+    CalendarModule,
+    RadioButtonModule,
+    InputNumberModule,CheckboxModule,InputTextareaModule
+  ],
+  exports: [SimpleReactiveFormComponent],
+})
+export class SharedModule {}
