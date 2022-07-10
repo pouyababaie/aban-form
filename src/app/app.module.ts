778f49399d47c71718e98ca5b9ea21abcc7b8b3a
pouyababaie <pouyababaie123@2gmail.com>
2022-07-10 15:47:05 +0430

    bases added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SimpleReactiveFormComponent } from './simple-reactive-form/simple-reactive-form.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [AppComponent, SimpleReactiveFormComponent],
-  imports: [BrowserModule, ReactiveFormsModule, FormsModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
